69e6a7a1692a0d4c8e45a663a2939784144fe156
Tim Qi <i@timqi.com>
2016-09-13 14:29:28 +0800

    update

---
+++ experience/Android Studio 使用.md
@@ -0,0 +1,5 @@
+## 跳过首次安装 SDK 下载
+
+在 bin 目录的 idea.properties 增加一行：
+
+  disable.android.first.run=true
---
+++ snippet/学位贵直播室刷聊天.js
@@ -0,0 +1,17 @@
+for (var i = 0; i < 1000; i++) {
+  $('#chatarea').html('qiqi '+i)
+  $("[name='toward']").val('submitChat')
+  sendType()
+}
+
+var i = 0;
+function send() {
+  i++;
+  $('#chatarea').html('qiqi '+i)
+  $("[name='toward']").val('submitChat')
+  sendType()
+}
+setInterval("send()",1000)
+
+setTimeout("send()",1000)
+ex
